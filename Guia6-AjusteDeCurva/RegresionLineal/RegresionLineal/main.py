e4949ae9179c1248e5eeb5e3665bf34831a18e26
Augusto Portillo <aportillo@desarrollosnea.com>
2019-05-04 20:10:12 -0300

    Calculo de linealizacion para hallar el valor teorico y comprar con el valor experimtal

--- Guia6-AjusteDeCurva/RegresionLineal/RegresionLineal/main.py
+++ Guia6-AjusteDeCurva/RegresionLineal/RegresionLineal/main.py
@@ -2,40 +2,54 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
+Ve = 10 #Voltios (V)
+R = 12000 #Ohm
+C = 0.00022 #Faradio (F)
 
-t = []
-V = []
+texp = [] #Tiempo experimental
+Vexp = [] #Voltaje experimental
+
+def evaluarTEnModelo(t):
+	prueba = np.exp(np.log(Ve) + (-t * 1/(R*C))) #Esto corresponde a la Ecuacion de la descarga del capacitor linealizada
+	return prueba
+
 def obtenerDatos():
     f = open("datos.txt", "r")
     i = int()
     aux = int(0)
-    
     for linea in f.readlines():
         linea = linea.split("   ")
         aux = aux +1
         for i in range (1,101):
             if aux <2:
                     a = float(linea[i])
-                    t.append(a)
+                    texp.append(a)
             if aux >1:
                     b = float(linea[i])
-                    V.append(b)
+                    Vexp.append(b)
+
 # Linea que mejor se ajusta a la curva
 obtenerDatos()
 sumxy = 0
 sumx2 = 0
-for index, item in enumerate(t):
-	sumxy += item*V[index]
+for index, item in enumerate(texp):
+	sumxy += item*Vexp[index]
 	sumx2 += item**2
 
-a1numerador = len(t) * sumxy - np.sum(t)*np.sum(V)
-a1denominador = len(t) * sumx2 - (np.sum(t) **2)
+a1numerador = len(texp) * sumxy - np.sum(texp)*np.sum(Vexp)
+a1denominador = len(texp) * sumx2 - (np.sum(texp) **2)
 
 a1 = a1numerador / a1denominador
-a0 = np.average(V) - a1* np.average(t)
+a0 = np.average(Vexp) - a1* np.average(texp)
 
 print('Ajuste Lineal')
 print('y = {} + {} x'.format(str(a0),str(a1)))
+
+# Comparacion de datos
+print('Valor Exp   Valor Teorico')
+for index, x in enumerate(texp):
+	aux = evaluarTEnModelo(x)
+	print('{}   {}'.format(str(Vexp[index]), str(aux)))
 
 # GRAFICAMOS
 # Create the vectors X and Y
@@ -44,5 +58,5 @@ y = a0 + a1*x
 
 # Create the plot
 plot.plot(x,y,label='Ajuste Lineal')
-plot.scatter(t, V)
+plot.scatter(texp, Vexp)
 plot.show()
